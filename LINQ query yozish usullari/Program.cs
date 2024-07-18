0c8ae4686391ea5082ace2b9b72e119c184708b2
Ramazon Tog'aymurodov <togaymurodovramazon179@gmail.com>
2024-07-18 20:05:06 +0500

    Add project files.

---
+++ LINQ query yozish usullari/Program.cs
@@ -0,0 +1,26 @@
+﻿
+List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+// Query syntax
+var querysyntax = from all in list
+                  where all>8
+                  select all;
+Console.WriteLine(" To'plamdagi 8 dan katta element ");
+foreach (var query in querysyntax)
+{
+    Console.Write( query +" ");
+}
+Console.WriteLine();
+
+// Method syntax
+var methodsyntax = list.Where(x => x < 8);
+Console.WriteLine(" To'plamdagi 8 dan kichik element ");
+foreach (var method in methodsyntax)
+{
+    Console.Write( method + " ");
+}
+Console.WriteLine();
+
+// Mixed syntax
+var mixedsyntax = (from all in list
+                   select all).Max();
+Console.Write(" To'plamdagi maxsimal element = " + mixedsyntax);
